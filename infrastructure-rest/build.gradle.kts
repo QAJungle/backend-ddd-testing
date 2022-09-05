f9a0d70ff15fba68fd2acd3e0c029b0102866e71
Aritz Aguila <aaguila@Aritzs-MacBook-Pro.local>
2022-09-05 15:55:27 +0200

    [feature/db-persistence] JPA persistence for rest application

--- infrastructure-rest/build.gradle.kts
+++ infrastructure-rest/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   id("org.springframework.boot") version "2.7.1"
   id("io.spring.dependency-management") version "1.0.11.RELEASE"
+
   kotlin("jvm")
   kotlin("plugin.spring") version "1.6.21"
 }
@@ -19,6 +20,7 @@ dependencies {
   implementation(project(":infrastructure"))
 
   implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.3")
   implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
--- infrastructure-shell/src/main/kotlin/com/qajungle/backendddtesting/infrastructure/console/config/book/Persistence.kt
+++ infrastructure-shell/src/main/kotlin/com/qajungle/backendddtesting/infrastructure/console/config/book/Persistence.kt
@@ -2,8 +2,11 @@ package com.qajungle.backendddtesting.infrastructure.console.config.book
 
 import com.qajungle.backendddtesting.domain.read.book.BookReader
 import com.qajungle.backendddtesting.domain.write.BookWriter
+import com.qajungle.backendddtesting.infrastructure.persistence.read.jpa.book.BookJPAReaderRepository
 import com.qajungle.backendddtesting.infrastructure.persistence.read.memory.book.BookViewMemory
 import com.qajungle.backendddtesting.infrastructure.persistence.write.console.book.BookWriterConsole
+import com.qajungle.backendddtesting.infrastructure.persistence.write.jpa.book.BookJPA
+import com.qajungle.backendddtesting.infrastructure.persistence.write.jpa.book.BookJPARepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -11,12 +14,12 @@ import org.springframework.context.annotation.Configuration
 class Persistence {
 
   @Bean
-  fun bookReader(): BookReader? {
+  fun bookReader(repository: BookJPAReaderRepository): BookReader? {
     return BookViewMemory()
   }
 
   @Bean
-  fun bookWriter(): BookWriter? {
-    return BookWriterConsole()
+  fun bookWriter(repository: BookJPARepository): BookWriter? {
+    return BookJPA(repository)
   }
 }
